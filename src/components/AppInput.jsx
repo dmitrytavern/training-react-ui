b53b2888b18f0c27e30f7f479ff5bb69046dfd51
dogger-work <dogger.work@gmail.com>
2021-01-11 00:08:51 +0200

    0.1v - Init project and start createing form-control components

---
+++ src/components/AppInput.jsx
@@ -0,0 +1,46 @@
+import './AppInput.sass'
+import React, { useState } from 'react'
+
+const AppInput = ({ id, label, type, error, errorMessage, value, onChange, onFocus }) => {
+	const [focus, setFocus] = useState(false)
+
+	const handlerFocus = () => {
+		setFocus(true)
+		onFocus()
+	}
+
+	const handlerBlur = () => {
+		setFocus(false)
+	}
+
+
+	let inputClassName = 'app-input'
+	if (error) inputClassName += ' app-input_error'
+	if (focus) inputClassName += ' app-input_focus'
+	if (focus || value.length !== 0) inputClassName += ' app-input_active'
+
+	return (
+		<div className={inputClassName}>
+			<div className="app-input__inner">
+
+				<label htmlFor={id} className="app-input__label">{label}</label>
+
+				<input
+					id={id}
+					className="app-input__input"
+					type={type}
+
+					value={value}
+					onFocus={handlerFocus}
+					onBlur={handlerBlur}
+					onChange={onChange}
+				/>
+
+			</div>
+
+			<div className="app-input__error">{errorMessage}</div>
+		</div>
+	);
+};
+
+export default AppInput;
