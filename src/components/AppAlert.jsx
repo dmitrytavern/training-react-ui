b9d7a203c01cafab337297f7a020eaedd8fc9f93
dogger-work <dogger.work@gmail.com>
2021-01-22 22:28:14 +0200

    0.1.8v - Added alert component

---
+++ src/components/AppAlert.jsx
@@ -0,0 +1,54 @@
+import './AppAlert.sass'
+import { useRef } from "react"
+import PropTypes from 'prop-types'
+import { CSSTransition } from "react-transition-group"
+import { ReactComponent as AppIconAlertDanger } from '../assets/img/icons/alert-danger.svg'
+import { ReactComponent as AppIconClose } from '../assets/img/icons/close.svg'
+
+
+const AppAlert = (props) => {
+	const alertRef = useRef(null)
+	const { type, show, content, onClose, children } = props
+	const theme = ' app-alert_' + (type || 'default')
+
+	let alertClassName = 'app-alert' + theme
+
+	return (
+		<CSSTransition
+			in={show}
+			nodeRef={alertRef}
+			classNames="app-alert"
+			timeout={300}
+			unmountOnExit
+		>
+			<div className={alertClassName} ref={alertRef}>
+				<div className="app-alert__inner">
+					<div className="app-alert__icon">
+						{ type === 'danger' && <AppIconAlertDanger /> }
+					</div>
+
+					<div className="app-alert__content">
+						{content || children}
+					</div>
+
+					<button
+						type="button"
+						className="app-alert__close"
+						onClick={onClose}
+					>
+						<AppIconClose />
+					</button>
+				</div>
+			</div>
+		</CSSTransition>
+	)
+}
+
+AppAlert.propTypes = {
+	type: PropTypes.string,
+	show: PropTypes.bool.isRequired,
+	content: PropTypes.any,
+	onClose: PropTypes.func.isRequired
+}
+
+export default AppAlert
--- src/pages/AppAuth.jsx
+++ src/pages/AppAuth.jsx
@@ -1,22 +1,29 @@
 import { useState } from 'react'
 import { AuthContext } from "../components/AppAuth/auth.context"
 
+import AppAlert from "../components/AppAlert"
 import AppLoader from "../components/AppLoader"
 import AppAuthForm from "../components/AppAuth/AppAuthForm"
 
 const AppAuth = () => {
+	const [error, setError] = useState(false)
+	const [errorText, setErrorText] = useState('')
 	const [loading, setLoading] = useState(false)
 
-	const onLogin = (props) => {
+	const onLogin = () => {
 		setLoading(true)
 
 		setTimeout(() => setLoading(false), 1000)
 	}
 
-	const onRegister = (props) => {
+	const onRegister = () => {
 		setLoading(true)
 
-		setTimeout(() => setLoading(false), 1000)
+		setTimeout(() => {
+			setLoading(false)
+			setError(true)
+			setErrorText('404: Page not found.')
+		}, 1000)
 	}
 
 
@@ -27,6 +34,16 @@ const AppAuth = () => {
 
 	return (
 		<AuthContext.Provider value={authContextValue}>
+			{/* Error alert */}
+			<AppAlert
+				type="danger"
+				show={error}
+				content={errorText}
+				onClose={() => setError(false)}
+			/>
+
+
+			{/* Form */}
 			<div style={{ maxWidth: "440px", padding: '30px', position: 'relative'}}>
 				<AppLoader loading={loading} />
 				<AppAuthForm />
