0ab9d4ee8e6ca7d78d283ca67e281f24b1c2dc39
dogger-work <dogger.work@gmail.com>
2021-01-24 18:06:01 +0200

    0.1.12v - Added focus to password button

--- src/components/AppCheckbox.jsx
+++ src/components/AppCheckbox.jsx
@@ -1,11 +1,29 @@
 import './AppCheckbox.sass'
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 import { ReactComponent as AppIconCheck } from '../assets/img/icons/check.svg'
 
 const AppCheckbox = (props) => {
 	const { id, value, error, onChange, onClick } = props
+	const [focus, setFocus] = useState(false)
+
+
+	const handlerKeyDown = (e) => {
+		if (e.key === 'Enter') {
+			onChange()
+		}
+	}
+
+	const handlerFocus = () => {
+		setFocus(true)
+	}
+
+	const handlerBlur = () => {
+		setFocus(false)
+	}
 
 	let checkboxClassName = 'app-checkbox'
+	if (focus) checkboxClassName += ' app-checkbox_focus'
 	if (error) checkboxClassName += ' app-checkbox_error'
 	if (value) checkboxClassName += ' app-checkbox_checked'
 
@@ -22,7 +40,14 @@ const AppCheckbox = (props) => {
 				{...props.inputProps}
 			/>
 
-			<label htmlFor={id} className="app-checkbox__label">
+			<label
+				htmlFor={id}
+				className="app-checkbox__label"
+				onFocus={handlerFocus}
+				onBlur={handlerBlur}
+				onKeyDown={handlerKeyDown}
+				tabIndex={0}
+			>
 				<span className="app-checkbox__box">
 					<span className="app-checkbox__box-icon">
 						<AppIconCheck />
